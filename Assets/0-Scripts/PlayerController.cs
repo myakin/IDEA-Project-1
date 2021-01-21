75f87a21753c44d8256bd89ef6fd9f103f780d76
myakin <yakin_murat@yahoo.com>
2021-01-21 18:36:49 +0300

    Part 3

--- Assets/0-Scripts/PlayerController.cs
+++ Assets/0-Scripts/PlayerController.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     public float moveSpeed = 0.001f;
     public float jumpStrength = 100f;
+    public float velocityForceMagnitude = 1;
 
     private Animator animator;
     private bool isJumping;
-    private float timer;
-    private bool runTimer;
-    private int jumpNumber;
     private bool isCheckingGround;
+    private float jumpTimer;
+    private Vector3 oldPosition;
+    private float moveMultiplier = 1;
 
     private void Start() {
         animator = GetComponent<Animator>();
@@ -24,49 +25,73 @@ public class PlayerController : MonoBehaviour {
 
         // Debug.Log(jumpValue);
         
-        animator.SetFloat("walkLeftRight", hor);
-        transform.position += transform.right * (moveSpeed * hor);
-        
-        if (hor>0) {
-            GetComponent<SpriteRenderer>().flipX = false;
-        } else if (hor<0) {
-            GetComponent<SpriteRenderer>().flipX = true;
-        }
-
-        if (jumpValue>0 && !isJumping && jumpNumber<2) {
-            isJumping = true;
-            runTimer = true;
-            jumpNumber++;
-            animator.SetBool("jump", true);
-            GetComponent<Rigidbody2D>().AddForce(transform.up * jumpStrength);
-        } else {
-            animator.SetBool("jump", false);
-        }
-
-        if (runTimer) {
-            timer+=Time.deltaTime;
-            if (timer>0.25f) {
-                isJumping = false;
-                isCheckingGround = true;
-                // timer = 0;
-                // runTimer=false;
+        if (!isJumping) {
+            if (Input.GetKey(KeyCode.LeftShift)) {
+                moveMultiplier = 3;
+                animator.SetBool("isRunning", true);
+            } else {
+                moveMultiplier = 1;
+                animator.SetBool("isRunning", false);
             }
-            if (timer>1) {
-                jumpNumber = 0;
-                runTimer = false;
-                timer = 0;
+
+            animator.SetFloat("walkLeftRight", hor * moveMultiplier);
+            transform.position += transform.right * (moveSpeed * hor * moveMultiplier);
+            
+            if (hor>0) {
+                GetComponent<SpriteRenderer>().flipX = false;
+            } else if (hor<0) {
+                GetComponent<SpriteRenderer>().flipX = true;
             }
         }
+
+        // jump trigger
+        if (jumpValue>0) {
+            if (!isJumping) {
+                isJumping = true;
+                animator.SetTrigger("triggerJump");
+
+                transform.SetParent(null);
+
+                Vector3 velocity = transform.position - oldPosition;
+                GetComponent<Rigidbody2D>().AddForce(velocity * velocityForceMagnitude);
+
+            }
+            jumpTimer+=Time.deltaTime;
+            if (jumpTimer<0.5f) {
+                GetComponent<Rigidbody2D>().AddForce(transform.up * jumpStrength);
+            }
+        } 
         
+        // ground checking
         if (isCheckingGround) {
             RaycastHit2D hit = Physics2D.Raycast(transform.position, -transform.up, 0.2f, 1<<0);
             if (hit.collider!=null && hit.collider.tag == "Platform") {
                 // Debug.Log(hit.collider.gameObject.name);
                 // set animator to idle
                 animator.SetBool("isGrounded", true);
-                isCheckingGround = false;
+                transform.SetParent(hit.transform);
             }
+
+            // if (animator.GetCurrentAnimatorStateInfo(0).IsName("CharacterIdleAnimation")) {
+            //     ResetJump();
+            // }
         }
+
+        oldPosition = transform.position;
         
     }
+
+    // called from CharacterJumpEndAnimation animation
+    public void ResetJump() {
+        isCheckingGround = false;
+        isJumping = false;
+        animator.SetBool("isGrounded", false);
+        jumpTimer = 0;
+        GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+    }
+
+    // called from CharacterJumpStartAnimation animation
+    public void StartCheckingGround() {
+        isCheckingGround = true;
+    } 
 }
